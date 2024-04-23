a5bff5870929c6b6c2e6de4a4abe87f6b56fde84
oguzhanyalcin <oyalcin@innova.com.tr>
2024-04-23 20:02:11 +0300

    commit

---
+++ Sources/DynamicDemoLib/Demo.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Oguzhan Yalcin on 23.04.2024.
+//
+
+import Foundation
+
+public class DemoClass {
+    
+    public static func getName() -> String {
+        return "Demo name v1.0.0"
+    }
+}
